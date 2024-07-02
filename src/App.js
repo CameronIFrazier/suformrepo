eef0b63dbc9aa86073a1b1094d7a9483872ecae5
CameronIFrazier <taterfrazier1@gmail.com>
2024-07-01 17:39:59 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import React from 'react';
+import bg from "./images/bg-intro-desktop.png";
 import './App.css';
 
 function App() {
+const [inputValue, setInputValue] = useState('');
+const [errorMessage, setErrorMessage] = useState('');
+
+const change = (e) => {
+const value = e.target.value;
+setInputValue(value);
+
+if(value === ''){
+setInputValue('Cannot be left Empty');
+}
+}
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='wrapper'>
+        <img src={bg} alt="" className='bgimage' />
+        <div className='boxcontainer'>
+          <div className='box1'>box1
+            <h2 className='header'>Learn to code by watching others</h2>
+            <p className='paragraph'>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, 
+            but understanding how developers think is invaluable. </p>
+          </div>
+          <div  className='box2'>box2 
+          <input type ="text"  name ="firstName" placeholder='First Name' className='firstName'></input>
+          <input type ="text" value={inputValue} onChange={change} name ="lastName" placeholder='Last Name' className='lastName' ></input>
+          <input type ="text"  name ="email" placeholder='email' className='email'></input>
+          <input type ="text"  name ="password" placeholder='password' className='password'></input>
+          <button className='claim'></button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
