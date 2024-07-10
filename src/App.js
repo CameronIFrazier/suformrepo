20acfaf35ba6636685ef1d26f0dae352a77d35e4
CameronIFrazier <taterfrazier1@gmail.com>
2024-07-10 01:10:17 -0700

    fix confusing states

--- src/App.js
+++ src/App.js
@@ -1,202 +1,107 @@
-import {useState} from 'react';
-import React from 'react';
+import { useState } from "react";
+import React from "react";
 import bg from "./images/bg-intro-desktop.png";
-import './App.css';
+import "./App.css";
 import error from "./images/icon-error.svg";
 
 function App() {
-const[fnvisibility, fnsetVisiblity] = useState({
-  visibility: 'hidden'
-})
-const[lnvisibility, lnsetVisiblity] = useState({
-  visibility: 'hidden'
-})
-const[emvisibility, emsetVisiblity] = useState({
-  visibility: 'hidden'
-})
-const[psvisibility, pssetVisiblity] = useState({
-  visibility: 'hidden'
-})
+  const [efn, setEfn] = useState();
 
-const[efn,setefn]=useState('')
-const [fninputStyle, fnsetInputStyle] =useState({
-  color: 'black',
-  borderColor: 'black',
-  width: '97%',
-  height: '2rem',
-  borderRadius: ".3rem"
-});
-const[eln,setln]=useState('')
-const [lninputStyle, lnsetInputStyle] =useState({
-  color: 'black',
-  borderColor: 'black',
-  width: '97%',
-  height: '2rem',
-  borderRadius: ".3rem"
-});
-const[em,setem]=useState('')
-const [eminputStyle, emsetInputStyle] =useState({
-  color: 'black',
-  borderColor: 'black',
-  width: '97%',
-  height: '2rem',
-  borderRadius: ".3rem"
-});
-const[ps,setps]=useState('')
-const [psinputStyle, pssetInputStyle] =useState({
-  color: 'black',
-  borderColor: 'black',
-  width: '97%',
-  height: '2rem',
-  borderRadius: ".3rem"
-});
+  const [eln, setln] = useState();
 
+  const [em, setem] = useState();
 
-const changefn = (e) => {
-const value = e.target.value;
-setefn(value);
-}
-const changeln = (e) => {
-  const value = e.target.value;
-  setln(value);
-  
-  }
+  const [ps, setps] = useState();
+
+  const changeEfn = (e) => {
+    console.log(e.target.value);
+    const value = e.target.value;
+
+    if (!value) {
+      setEfn("");
+    }
+
+    setEfn(value);
+  };
+  const changeln = (e) => {
+    const value = e.target.value;
+    setln(value);
+  };
   const changeem = (e) => {
     const value = e.target.value;
     setem(value);
-    }
-    const changeps = (e) => {
-      const value = e.target.value;
-      setps(value);
-      }
+  };
+  const changeps = (e) => {
+    const value = e.target.value;
+    setps(value);
+  };
 
-const submittedEmpty = (e) => {
-if(efn === ''){
-  fnsetInputStyle({
-    color: 'red',
-    borderColor: 'red',
-    width: '97%',
-    height: '2rem',
-    borderRadius: ".3rem"
-    
-  })
-  fnsetVisiblity({
-    visibility: "visible"
-  })
-}else{
-  fnsetInputStyle({
-    color: 'black',
-    borderColor: 'black',
-    width: '97%',
-    height: '2rem',
-    borderRadius: ".3rem"
-  })
-  fnsetVisiblity({
-    visibility: "hidden"
-  })
-}
-if(eln === ''){
-  lnsetInputStyle({
-    color: 'red',
-    borderColor: 'red',
-    width: '97%',
-    height: '2rem',
-    borderRadius: ".3rem"
-  })
-  lnsetVisiblity({
-    visibility: "visible"
-  })
-}else{
-  lnsetInputStyle({
-    color: 'black',
-    borderColor: 'black',
-    width: '97%',
-    height: '2rem',
-    borderRadius: ".3rem"
-  })
-  lnsetVisiblity({
-    visibility: "hidden"
-  })
-}
-if(em === ''){
-  emsetInputStyle({
-    color: 'red',
-    borderColor: 'red',
-    width: '97%',
-    height: '2rem',
-    borderRadius: ".3rem"
-  })
-  emsetVisiblity({
-    visibility: "visible"
-  })
-}else{
-  emsetInputStyle({
-    color: 'black',
-    borderColor: 'black',
-    width: '97%',
-    height: '2rem',
-    borderRadius: ".3rem"
-  })
-  emsetVisiblity({
-    visibility: "hidden"
-  })
-}
-if(ps === ''){
-  pssetInputStyle({
-    color: 'red',
-    borderColor: 'red',
-    width: '97%',
-    height: '2rem',
-    borderRadius: ".3rem"
-  })
-  pssetVisiblity({
-    visibility: "visible"
-  })
-}else{
-  pssetInputStyle({
-    color: 'black',
-    borderColor: 'black',
-    width: '97%',
-    height: '2rem',
-    borderRadius: ".3rem"
-  })
-  pssetVisiblity({
-    visibility: "hidden"
-  })
-}
-
-}
-
-
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <>
-      <div className='wrapper'>
-        <img src={bg} alt="" className='bgimage' />
-       <div className='grid-middle'>
-        <div className='emptyBoxLol'> </div>
-        <button className='trialButton'>Try it free 7 days then 20/month. thereafter </button>
+      <div className="container">
+        <div className="grid-middle">
+          <div className="emptyBoxLol"> </div>
+          <button className="trialButton">
+            Try it free 7 days then 20/month. thereafter{" "}
+          </button>
 
-          <div className='box1' >
-            <h2 className='header'>Learn to code by watching others</h2>
-            <p className='paragraph'>See how experienced developers solve problems in real-time. Watching scripted tutorials is great, 
-            but understanding how developers think is invaluable. </p>
+          <div className="box1">
+            <h2 className="header">Learn to code by watching others</h2>
+            <p className="paragraph">
+              See how experienced developers solve problems in real-time.
+              Watching scripted tutorials is great, but understanding how
+              developers think is invaluable.{" "}
+            </p>
           </div>
 
-          <div  className='box2'> 
-          <input type ="text" style={fninputStyle}  onChange={changefn} name ="firstName" placeholder='First Name' className='firstName'></input>
-          <div className='lilRedText' style={fnvisibility}>First Name cannot be empty </div>
-          <input type ="text" style={lninputStyle}  onChange={changeln} name ="lastName" placeholder='Last Name' className='lastName' ></input>
-          <div className='lilRedText' style={lnvisibility}>Last Name cannot be empty</div>
-          <input type ="text" style={eminputStyle}  onChange={changeem}  name ="email" placeholder='Email' className='email'></input>
-          <div className='lilRedText' style={emvisibility}>Email cannot be empty</div>
-          <input type ="text" style={psinputStyle}  onChange={changeps}  name ="password" placeholder='Password' className='password'></input>
-          <div className='lilRedText' style={psvisibility}>Password cannot be empty</div>
-          <button onClick = {submittedEmpty} className='claim'> Claim Your Free Trial</button>
-          </div>
-          
-          </div> 
+          <form className="box2">
+            <input
+              type="text"
+              className={`${efn === "" ? "error" : "normalState"}`}
+              onChange={changeEfn}
+              name="firstName"
+              placeholder="First Name"
+              value={efn}
+            />{" "}
+            <div className={`lilRedText ${efn === "" ? "invis" : ""}`}>
+              First Name cannot be empty{" "}
+            </div>
+            <input
+              type="text"
+              className={eln === "" ? "error" : "normalState"}
+              onChange={changeln}
+              name="lastName"
+              placeholder="Last Name"
+            ></input>
+            <div className="lilRedText">Last Name cannot be empty</div>
+            <input
+              type="text"
+              className={`${em === "" ? "error" : "normalState"}`}
+              onChange={changeem}
+              name="email"
+              placeholder="Email"
+            ></input>
+            <div className="lilRedText">Email cannot be empty</div>
+            <input
+              type="text"
+              className={`${ps === "" ? "error" : "normalState"}`}
+              onChange={changeps}
+              name="password"
+              placeholder="Password"
+            ></input>
+            <div className="lilRedText">Password cannot be empty</div>
+            <button type="submit" onClick={handleSubmit} className="claim">
+              Claim Your Free Trial
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className="wrapper">
+        <img src={bg} alt="" className="bgimage" />
       </div>
     </>
   );
